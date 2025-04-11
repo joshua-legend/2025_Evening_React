b3d24c74977004526895241862a3e817f37c6b27
joshua <iuttn123@gmail.com>
2025-04-11 19:53:23 +0900

    todo effect

--- todolist/src/featured/calendar/Calendar.jsx
+++ todolist/src/featured/calendar/Calendar.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { CurrentTime } from "../../shared/util/whatTime";
 import styles from "./Calendar.module.css";
 import TimeText from "./components/TimeText";
+import ClockText from "./components/ClockText";
 
 const Calendar = () => {
   return (
-    <div className={styles.layout}>
-      {CurrentTime().map((v) => (
-        <TimeText text={v} />
-      ))}
+    <div className={styles.parents_layout}>
+      <div className={styles.layout}>
+        {CurrentTime().map((v) => (
+          <TimeText text={v} />
+        ))}
+      </div>
+      <ClockText />
     </div>
   );
 };
